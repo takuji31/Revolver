53331c0746a58ee7ee5a97b6c2fb8cec80052be3
Nishibayashi Takuji <takuji31@gmail.com>
2013-05-13 03:29:27 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Revolver - It's new $module
+
+# SYNOPSIS
+
+    use Revolver;
+
+# DESCRIPTION
+
+Revolver is ...
+
+# LICENSE
+
+Copyright (C) Nishibayashi Takuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Nishibayashi Takuji <takuji31@gmail.com>
---
+++ lib/Revolver.pm
@@ -0,0 +1,39 @@
+package Revolver;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Revolver - It's new $module
+
+=head1 SYNOPSIS
+
+    use Revolver;
+
+=head1 DESCRIPTION
+
+Revolver is ...
+
+=head1 LICENSE
+
+Copyright (C) Nishibayashi Takuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji31@gmail.comE<gt>
+
+=cut
+
